dacdc7ef501ed9f1f84b2e0c4f6b52efba37da38
Andrew Knowles <ajknowles@optonline.net>
2024-08-17 15:28:37 -0400

    kinda working moon orbits

--- GMTK-Game-Jam-2024/Assets/AndrewTestDump/OrbitalBody.cs
+++ GMTK-Game-Jam-2024/Assets/AndrewTestDump/OrbitalBody.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 enum InitialVelocityType
 {
@@ -12,48 +14,108 @@ enum InitialVelocityType
 [RequireComponent(typeof(Rigidbody))]
 public class OrbitalBody : MonoBehaviour
 {
-	private static float G = 0.0001f; //make this handled in another script
+	private static float G = 0.0001f; //make this handled in another script perhaps
 
-	[SerializeField] private Rigidbody _parent;
+	[SerializeField] private List<OrbitalBody> _parents;
 
 	[SerializeField] private InitialVelocityType _initVelocityType = InitialVelocityType.AutoCircular;
 	[SerializeField] private Vector3 _initVelocity;
 
 	private Rigidbody _body;
-
-
-	// Start is called before the first frame update
-	void Start()
+	public Rigidbody body
 	{
-		_body = GetComponent<Rigidbody>();
-		Vector3 difference = _parent.position - _body.position;
-		Vector3 gravityDirection = difference.normalized;
-		float distance = difference.magnitude;
-
-		Vector3 direction = Vector3.Cross(_parent.transform.up, gravityDirection);
-		switch (_initVelocityType)
+		get
 		{
-			case InitialVelocityType.AutoCircular:
-				_body.AddForce(direction * Mathf.Sqrt(G * _parent.mass / distance), ForceMode.VelocityChange);
-				break;
-			case InitialVelocityType.SetSpeed:
-				_body.AddForce(direction * _initVelocity.x, ForceMode.VelocityChange);
-				break;
-			case InitialVelocityType.SetVelocity:
-				_body.AddForce(_initVelocity, ForceMode.VelocityChange);
-				break;
+			if (_body) return _body;
+			return _body = GetComponent<Rigidbody>();
 		}
 	}
 
-	void FixedUpdate()
+	private bool _initialLaunchHandled = false;
+	[DoNotSerialize] public Vector3 InitialLaunchVelocity;
+
+	// Start is called before the first frame update
+	private void Start()
 	{
-		Vector3 difference = _parent.position - _body.position;
+		foreach (var parent in _parents)
+		{
+			parent.InitialLaunch();
+		}
+
+		InitialLaunch();
+	}
+
+	public void InitialLaunch()
+	{
+		if (_initialLaunchHandled)
+		{
+			return;
+		}
+		if (_parents.Count == 0)
+		{
+			if (_initVelocityType != InitialVelocityType.SetVelocity)
+			{
+				Debug.LogWarning("Parentless OrbitalBody requires an initial velocity type of SetVelocity");
+				_initVelocityType = InitialVelocityType.SetVelocity;
+			}
+			InitialLaunchVelocity = _initVelocity;
+			body.AddForce(InitialLaunchVelocity, ForceMode.VelocityChange);
+		}
+		else
+		{
+			switch (_initVelocityType)
+			{
+				case InitialVelocityType.AutoCircular:
+				{
+					foreach (var parent in _parents)
+					{
+						Vector3 difference = parent.body.position - body.position;
+						Vector3 gravityDirection = difference.normalized;
+						float distance = difference.magnitude;
+
+						Vector3 direction = Vector3.Cross(parent.transform.up, gravityDirection);
+						InitialLaunchVelocity += direction * Mathf.Sqrt(G * parent.body.mass / distance);
+					}
+					break;
+				}
+				case InitialVelocityType.SetSpeed:
+				{
+					Vector3 difference = _parents[0].body.position - body.position;
+					Vector3 gravityDirection = difference.normalized;
+					Vector3 direction = Vector3.Cross(_parents[0].transform.up, gravityDirection);
+					InitialLaunchVelocity = direction * _initVelocity.x;
+					break;
+				}
+				case InitialVelocityType.SetVelocity:
+				{
+					InitialLaunchVelocity = _initVelocity;
+					break;
+				}
+			}
+
+			body.AddForce(InitialLaunchVelocity, ForceMode.VelocityChange);
+		}
+
+		_initialLaunchHandled = true;
+	}
+
+	private void FixedUpdate()
+	{
+		foreach (var parent in _parents)
+		{
+			ApplyGravityFrom(parent);
+		}
+	}
+
+	private void ApplyGravityFrom(OrbitalBody parent)
+	{
+		Vector3 difference = parent.body.position - body.position;
 		float distance = difference.magnitude;
 
-		float forceMagnitude = G * _parent.mass * _body.mass / Mathf.Pow(distance, 2);
-		
+		float forceMagnitude = G * parent.body.mass * body.mass / Mathf.Pow(distance, 2);
+
 		Vector3 forceDirection = difference.normalized;
 		Vector3 forceVector = forceDirection * forceMagnitude;
-		_body.AddForce(forceVector);
+		body.AddForce(forceVector);
 	}
 }
