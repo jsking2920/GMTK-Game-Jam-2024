915fbb8878d1f5118444959d42b242691b0244ec
Andrew Knowles <ajknowles@optonline.net>
2024-08-17 21:48:48 -0400

    Basic black hole mechanic

---
+++ GMTK-Game-Jam-2024/Assets/Scripts/BlackHole.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		var body = other.GetComponent<OrbitalBody>();
+		if (body)
+		{
+			if (other.CompareTag("CueBall")) //or whatever
+			{
+				HandleCueBall(body);
+			}
+			else
+			{
+				HandleBall(body);
+			}
+		}
+	}
+
+	private void HandleBall(OrbitalBody body)
+	{
+		body.gameObject.SetActive(false);
+		//communicate with score tracking somehow
+	}
+
+	private void HandleCueBall(OrbitalBody body)
+	{
+		//move to start or something
+	}
+}
