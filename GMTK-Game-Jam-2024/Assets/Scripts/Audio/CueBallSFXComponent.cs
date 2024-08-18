14764db72a99990a167a6eb2093ea8ce4b691c00
Will Ozeas <willatme@me.com>
2024-08-17 18:00:24 -0700

    Cueball Sfx

---
+++ GMTK-Game-Jam-2024/Assets/Scripts/Audio/CueBallSFXComponent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class CueBallSFXComponent : MonoBehaviour
+{
+    private EventInstance sparkle;
+    public float maxSparkleSpeed = 25;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sparkle = RuntimeManager.CreateInstance("event:/SFX/Sparkle");
+        sparkle.setParameterByName("SparklePercent", 0);
+        sparkle.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sparklePercent = Mathf.Clamp01(rb.velocity.magnitude / maxSparkleSpeed);
+        sparkle.setParameterByName("SparklePercent", sparklePercent);
+        Debug.Log(sparklePercent);
+    }
+}
