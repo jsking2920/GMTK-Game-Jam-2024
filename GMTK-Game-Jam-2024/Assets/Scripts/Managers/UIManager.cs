bbb6c2ff757df6c505fc2a07ec7081ab367eb9c9
ADJacent <93837784+ADJ-acent@users.noreply.github.com>
2024-08-20 04:25:15 -0700

    credits done

--- GMTK-Game-Jam-2024/Assets/Scripts/Managers/UIManager.cs
+++ GMTK-Game-Jam-2024/Assets/Scripts/Managers/UIManager.cs
@@ -5,7 +5,6 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static Unity.VisualScripting.Metadata;
 
 public class UIManager : MonoBehaviour
 {
@@ -14,6 +13,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject MainMenu;
     [SerializeField] private GameObject MainUICanvas;
     [SerializeField] private GameObject LevelSelectCanvas;
+    [SerializeField] private GameObject CreditsCanvas;
     [SerializeField] private GameObject wallParent;
     
     [SerializeField] private Button mainMenuStartButton;
@@ -21,11 +21,13 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Button quitButton;
     [SerializeField] private Button restartButton;
     [SerializeField] private Button levelsButton;
+    [SerializeField] private Button creditsButton;
     [SerializeField] private Button homeButton;
     [SerializeField] private Button settingsButton;
 
     [SerializeField] private TextMeshProUGUI shotCountText;
     [SerializeField] private Button levelBackToMenuButton;
+    [SerializeField] private Button creditsBackToMenuButton;
 
     bool isSettingsOpen = false;
     [SerializeField] private List<Button> ListSelectButtons;
@@ -51,6 +53,8 @@ public class UIManager : MonoBehaviour
         restartButton.onClick.AddListener(btn_Restart);
         levelsButton.onClick.RemoveAllListeners();
         levelsButton.onClick.AddListener(btn_Levels);
+        creditsButton.onClick.RemoveAllListeners();
+        creditsButton.onClick.AddListener(btn_Credits);
 //#if UNITY_WEBGL
         quitButton.gameObject.SetActive(false);
         //#endif
@@ -58,7 +62,6 @@ public class UIManager : MonoBehaviour
         mainMenuStartButton.onClick.RemoveAllListeners();
         mainMenuStartButton.onClick.AddListener(GameManager.Instance.StartGame);
         mainMenuStartButton.onClick.AddListener(playSFX);
-        ;
 
         homeButton.onClick.RemoveAllListeners();
         homeButton.onClick.AddListener(btn_Home);
@@ -69,12 +72,17 @@ public class UIManager : MonoBehaviour
         levelBackToMenuButton.onClick.RemoveAllListeners();
         levelBackToMenuButton.onClick.AddListener(btn_LevelReturn);
 
+        creditsBackToMenuButton.onClick.RemoveAllListeners();
+        creditsBackToMenuButton.onClick.AddListener(btn_CreditsReturn);
+
         homeButton.gameObject.SetActive(false);
         restartButton.gameObject.SetActive(false);
+
 
         shotCountText.text = "Shots: 0";
         MainUICanvas.SetActive(false);
         LevelSelectCanvas.SetActive(false);
+        CreditsCanvas.SetActive(false);
 
         for (int i = 0; i < ListSelectButtons.Count; ++i)
         {
@@ -134,7 +142,20 @@ public class UIManager : MonoBehaviour
 
     public void btn_LevelReturn()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/ButtonClick");
         LevelSelectCanvas.SetActive(false);
+    }
+
+    private void btn_Credits()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/ButtonClick");
+        CreditsCanvas.SetActive(true);
+    }
+
+    private void btn_CreditsReturn()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/ButtonClick");
+        CreditsCanvas.SetActive(false);
     }
 
     public void SetMainMenuActive(bool active)
